7ec19f8bbdcb1cdb64414d4f654180dd5c539c48
Rahul S <rsangole@apple.com>
2023-11-06 09:19:45 -0800

    more code and presentation

--- code/01-basics.R
+++ code/01-basics.R
@@ -1,116 +1,20 @@
 # setup ----
-
 library(dplyr)
-library(dbplyr)
 library(arrow)
-library(duckdb)
+library(tictoc)
 
 # read data ----
 nyc <- open_dataset(here::here("data/nyc_part"))
 nyc
 
+# investigate ----
+
 object.size(nyc)
+
+class(nyc)
 
 str(nyc)
 
+schema(nyc)
 
-
-
-nyc |>
-    filter(year %in% 2017:2021) |>
-    group_by(year) |>
-    summarize(
-        all_trips = n(),
-        shared_trips = sum(passenger_count > 1, na.rm = TRUE)
-    ) |>
-    mutate(pct_shared = shared_trips / all_trips * 100)
-
-library(tictoc)
-tic()
-nyc |>
-    group_by(year) |>
-    to_duckdb() |>
-    count(rate_code) |>
-    # show_query()
-    collect()
-toc()
-
-tic()
-nyc |>
-    group_by(year) |>
-    # to_duckdb() |>
-    count(rate_code) |>
-    # show_query()
-    collect()
-toc()
-
-
-# head(), select(), filter(), and collect() to preview results
-#
-nyc |>
-    # to_duckdb() |>
-    filter(year == 2020) |>
-    mutate(fare_pounds = fare_amount * 0.79) |>
-    select(fare_amount, fare_pounds) |>
-    # head() |>
-    # show_query() |>
-    collect()
-
-
-
-taxis_gbp <- nyc_taxi |>
-    mutate(across(ends_with("amount"), list(pounds = ~ .x * 0.79)))
-
-taxis_gbp
-
-
-taxis_gbp |>
-    select(contains("amount")) |>
-    head() |>
-    collect()
-
-
-
-
-# The arrow package contains methods for 37 dplyr table functions, many of which
-# are "verbs" that do transformations to one or more tables. The package also
-# has mappings of 211 R functions to the corresponding functions in the Arrow
-# compute library
-
-# slice
-long_rides_2021 <- nyc_taxi |>
-    filter(year == 2021 & trip_distance > 100) |>
-    select(pickup_datetime, year, trip_distance)
-
-long_rides_2021 |>
-    slice(1:3)
-
-
-long_rides_2021 |>
-    slice_max(n = 3, order_by = trip_distance, with_ties = FALSE) |>
-    collect()
-
-
-
-# pivot
-library(duckdb)
-
-nyc_taxi |>
-    group_by(vendor_name) |>
-    summarise(max_fare = max(fare_amount)) |>
-    to_duckdb() |> # send data to duckdb
-    pivot_longer(!vendor_name, names_to = "metric") |>
-    to_arrow() |> # return data back to arrow
-    collect()
-
-
-
-# functions within arrow
-nyc_taxi |>
-    mutate(vendor_name = na_if(vendor_name, "CMT")) |>
-    head() |>
-    collect()
-
-nyc_taxi |>
-    filter(str_ends(vendor_name, "S"), year == 2020, month == 9) |>
-    collect()
+nyc |> nrow()
--- code/02-usage-examples.R
+++ code/02-usage-examples.R
@@ -1,16 +1,105 @@
-# Verbs
+# Data Manipulation ----
+query <- nyc |>
+    filter(year == 2019) |>
+    select(fare_amount, passenger_count)
+query
 
+class(query)
 
+schema(query)
 
-# Joins
+# Collecting results ----
+query |>
+    collect()
 
+# Example of head() ----
+nyc |>
+    filter(year == 2019) |>
+    select(fare_amount, passenger_count) |>
+    head() |>
+    collect()
 
+# Filter using multiple conditions ----
+tic()
+nyc |>
+    filter(
+        str_ends(vendor_name, "S"),
+        year %in% c(2020, 2021), 
+        month == 9) |>
+    collect()
+toc()xw
+
+# Selecting columns by name ----
+nyc |>
+    select(
+        contains('pickup'),
+        ends_with('amount')
+    )
+
+# Selecting columns by type ----
+nyc |> 
+    select(
+        where(is.character)
+    )
+
+# Distinct ----
+tic()
+nyc |>
+    distinct(year, month) |>
+    arrange(year, month) |>
+    collect()
+toc()
+
+# Mutating columns ----
+tic()
+nyc |>
+    filter(year == 2019) |>
+    mutate(tip_percentage = tip_amount / fare_amount * 100) |>
+    select(fare_amount, tip_amount, tip_percentage) |>
+    head() |>
+    collect()
+toc()
+
+# Grouping & Aggregation ----
+
+## What's the average fare for each month? ----
+tic()
+nyc |>
+    group_by(month) |>
+    summarize(
+        mean_fare = mean(fare_amount, na.rm = TRUE)
+    ) |>
+    arrange(month) |>
+    collect()
+toc()
+
+## What percentage of taxi rides each year had more than 1 passenger? ----
+tic()
+nyc |>
+    filter(year %in% 2017:2021) |>
+    group_by(year) |>
+    summarize(
+        all_trips = n(),
+        shared_trips = sum(passenger_count > 1, na.rm = TRUE)
+    ) |>
+    mutate(pct_shared = shared_trips / all_trips * 100) |>
+    collect()
+toc()
+
+## What's the most common rate code for each year? ----
+tic()
+nyc |>
+    group_by(year) |>
+    count(rate_code) |>
+    collect()
+toc()
+
+# Joins ----
 
 nyc_taxi_zones <-
     read_csv_arrow(here::here("data/taxi_zone_lookup.csv")) |>
     select(location_id = LocationID,
            borough = Borough)
-
 nyc_taxi_zones
 
 nyc_taxi_zones_arrow <- arrow_table(
@@ -26,23 +115,40 @@ dropoff <- nyc_taxi_zones_arrow |>
     select(dropoff_location_id = location_id,
            dropoff_borough = borough)
 
-
-
-library(tictoc)
-
 tic()
-borough_counts <- nyc_taxi |>
+borough_counts <- nyc |>
     left_join(pickup) |>
     left_join(dropoff) |>
     count(pickup_borough, dropoff_borough) |>
     arrange(desc(n)) |>
     collect()
 toc()
+borough_counts
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 # DuckDB
-
 ## Window Functions
 
 fare_by_year |>
@@ -55,29 +161,43 @@ fare_by_year |>
     collect()
 
 
-# UDFs
 
 
-# Write a user-defined function which wraps the stringr function
-# str_replace_na(), and use it to replace any NA values in the vendor_name
-# column with the string “No vendor” instead. (Test it on the data from 2019 so
-# you’re not pulling everything into memory)
 
-register_scalar_function(
-    name = "replace_vendor_na",
-    function(context, string) {
-        stringr::str_replace_na(string, "No vendor")
-    },
-    in_type = schema(string = string()),
-    out_type = string(),
-    auto_convert = TRUE
-)
+taxis_gbp <- nyc_taxi |>
+    mutate(across(ends_with("amount"), list(pounds = ~ .x * 0.79)))
 
-vendor_names_fixed <- nyc_taxi |>
-    mutate(vendor_name = replace_vendor_na(vendor_name))
+taxis_gbp
 
-# Preview the distinct vendor names to check it's worked
-vendor_names_fixed |>
-    filter(year == 2019) |> # smaller subset of the data
-    distinct(vendor_name) |>
+
+taxis_gbp |>
+    select(contains("amount")) |>
+    head() |>
     collect()
+
+# The arrow package contains methods for 37 dplyr table functions, many of which
+# are "verbs" that do transformations to one or more tables. The package also
+# has mappings of 211 R functions to the corresponding functions in the Arrow
+# compute library
+
+# slice
+long_rides_2021 <- nyc_taxi |>
+    filter(year == 2021 & trip_distance > 100) |>
+    select(pickup_datetime, year, trip_distance)
+
+long_rides_2021 |>
+    slice(1:3)
+
+
+long_rides_2021 |>
+    slice_max(n = 3, order_by = trip_distance, with_ties = FALSE) |>
+    collect()
+
+
+
+# functions within arrow
+nyc_taxi |>
+    mutate(vendor_name = na_if(vendor_name, "CMT")) |>
+    head() |>
+    collect()
+
